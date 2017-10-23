b2c9a18c91819eec5548115f6c8b990c3c88806a
Apoorva Shukla <apshuk17@gmail.com>
2017-10-23 07:59:14 +0530

    Popilating database

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const firebaseConfig = {
+    apiKey: 'AIzaSyA02kVVuMcHlZF0XsRPCe3PMYYSCUc-Q0w',
+    authDomain: 'pepper-51555.firebaseapp.com',
+    databaseURL: 'https://pepper-51555.firebaseio.com',
+    projectId: 'pepper-51555',
+    storageBucket: 'pepper-51555.appspot.com',
+    messagingSenderId: '736165304321'
+  };
